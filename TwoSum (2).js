96096196f9c8419de296c77658ddcb39690293ae
Preston Porter <prestonp2018@gmail.com>
2019-05-28 19:11:55 -0500

    Add files via upload

---
+++ TwoSum (2).js
@@ -0,0 +1,22 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(nums, target) {
+    var result=[];
+    for(i=0; i < nums.length; i++)
+        {
+            for(j=i+1; j <nums.length; j++)
+                {
+                  t=  nums[i]+nums[j];
+                    if(t == target)
+                        {
+                            
+                             result.push(i);
+                             result.push(j);
+                            return result;
+                        }
+                }
+        }
+};
